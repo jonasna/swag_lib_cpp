984828df0e97cf2028a771f2d52b2be2ba70a657
jonasna <jonasna93@gmail.com>
2018-06-24 14:16:07 +0200

    added detatch and remove. Progress with timer thread function

--- timer_lib/timer.cpp
+++ timer_lib/timer.cpp
@@ -1,6 +1,6 @@
 #include "timer.h"
 
-timer::timer(int ms_period) : period_(ms_period), running_(false), next_id_(invalid_id + 1)
+timer::timer(int ms_period) : next_id_(invalid_id + 1), period_(ms_period), running_(false)
 {
 
 }
@@ -16,7 +16,40 @@ timer::timer_attachment_id timer::attach_handler(handler_type handler)
 	auto id = next_id_++;
 	delegates_.emplace_back(timer_delegate(id, std::move(handler)));
 
-	return timer_attachment_id();
+	return id;
+}
+
+bool timer::detach_handler(timer_attachment_id id)
+{
+	scoped_lock lock(lock_);
+	auto target = std::find(delegates_.begin(), delegates_.end(), timer_delegate(id));
+	return remove_timer_delegate(lock, target);
+}
+
+bool timer::remove_timer_delegate(scoped_lock& lock, std::vector<timer_delegate>::iterator& iter)
+{
+	if (iter == delegates_.end())
+		return false;
+
+	auto& target_delegate = *iter;
+
+	if(target_delegate.active_)
+	{
+		// The delegate is being invoked
+
+		// Request removal
+		target_delegate.active_ = false;
+
+		// Set and wait for handler to finish execution
+		// Handler will remove the delegate
+		target_delegate.wait_conditional.reset(new condition_var);
+		target_delegate.wait_conditional->wait(lock);
+	}else
+	{
+		delegates_.erase(iter);
+	}
+
+	return true;
 }
 
 void timer::timer_thread_worker()
@@ -27,10 +60,8 @@ void timer::timer_thread_worker()
 	{
 		signaler_.wait_for(lock, period_);
 
-		for (auto i = 0; i < delegates_.size(); ++i)
+		for (auto& delegate : delegates_)
 		{
-
-			auto& delegate = delegates_[i];
 			delegate.active_ = true;
 
 			// Release and invoke
@@ -43,6 +74,11 @@ void timer::timer_thread_worker()
 				
 			}else
 			{
+				// Someone requested the removal of the delegate...
+
+				// The thread who requested the removal is waiting for
+				// a signal
+				delegate.wait_conditional->notify_all();
 				
 			}
 
@@ -50,12 +86,17 @@ void timer::timer_thread_worker()
 	}
 }
 
+
+/*
+ *	Below section describes the implementation of timer_delegate
+ */
+
 timer::timer_delegate::timer_delegate(timer_attachment_id id) : info_id_(id), active_(false)
 {
 }
 
 timer::timer_delegate::timer_delegate(timer_delegate && other) noexcept
-	: info_id_(std::move(other.info_id_)), target_handler_(std::move(other.target_handler_)), active_(std::move(other.active_))
+	: info_id_(other.info_id_), target_handler_(std::move(other.target_handler_)), active_(other.active_)
 {
 }
 
@@ -63,11 +104,10 @@ timer::timer_delegate& timer::timer_delegate::operator=(timer_delegate && other)
 {
 	if(this != &other)
 	{
-		info_id_ = std::move(other.info_id_);
+		info_id_ = other.info_id_;
 		target_handler_ = std::move(other.target_handler_);
-		active_ = std::move(other.active_);
+		active_ = other.active_;
 	}
-
 	return *this;
 }
 
--- timer_lib/timer.h
+++ timer_lib/timer.h
@@ -6,8 +6,6 @@ class timer
 {
 
 	using condition_var = std::condition_variable;
-	using clock = std::chrono::steady_clock;
-	using time_stamp = std::chrono::time_point<clock>;
 	using duration = std::chrono::milliseconds;
 	using lock = std::mutex;
 	using scoped_lock = std::unique_lock<lock>;
@@ -21,6 +19,12 @@ public:
 
 	explicit timer(int ms_period);
 	~timer();
+
+	// Remove copy and move assignment/construction
+	timer(timer&&) = delete;
+	timer& operator=(timer&&) = delete;
+	timer(timer const&) = delete;
+	timer& operator=(timer const&) = delete;
 
 	timer_attachment_id attach_handler(handler_type handler);
 	bool detach_handler(timer_attachment_id id);
@@ -43,13 +47,22 @@ private:
 		timer_delegate& operator=(timer_delegate&& other) noexcept;
 		timer_delegate(timer_attachment_id id, handler_type handler) noexcept;
 
+		timer_delegate(timer_delegate const&) = delete;
+		timer_delegate& operator=(timer_delegate const&) = delete;
+
+		~timer_delegate() = default;
+
+		bool operator==(const timer_delegate& other) const;
+
 		timer_attachment_id info_id_;
 		handler_type target_handler_;
-		bool operator==(const timer_delegate& other) const;
 		bool active_;
+
 		// For synchronization on removal
 		std::unique_ptr<condition_var> wait_conditional;
 	};
+
+	bool remove_timer_delegate(scoped_lock& lock, std::vector<timer_delegate>::iterator& iter);
 
 	timer_attachment_id next_id_;
 	std::vector<timer_delegate> delegates_;
@@ -60,6 +73,5 @@ private:
 	condition_var signaler_;
 	std::thread worker_;
 	void timer_thread_worker();
-
 };
 
